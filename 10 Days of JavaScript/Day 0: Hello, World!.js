f2f5aa22d49a2a8521940840d2269360eb696d77
Neha Gadge <gadge.neha@gmail.com>
2021-01-20 14:42:40 -0800

    Update Day 0: Hello, World!.js

--- 10 Days of JavaScript/Day 0: Hello, World!.js
+++ 10 Days of JavaScript/Day 0: Hello, World!.js
@@ -1,15 +1,6 @@
-// ========================
-//       Information
-// ========================
+// Question: https://www.hackerrank.com/challenges/js10-hello-world/problem
 
-// Direct Link: https://www.hackerrank.com/challenges/js10-hello-world/problem
-// Difficulty: Easy
-// Max Score: 10
-// Language: JavaScript (Node.js)
-
-// ========================
-//         Solution
-// ========================
+//Solution
 
 function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
