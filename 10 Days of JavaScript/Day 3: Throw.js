3300feadea35d9d51f23027d1c81b0e832a549ee
Neha Gadge <gadge.neha@gmail.com>
2021-01-20 14:59:58 -0800

    Create Day 3: Throw.js

---
+++ 10 Days of JavaScript/Day 3: Throw.js
@@ -0,0 +1,16 @@
+//Question : https://www.hackerrank.com/challenges/js10-throw/problem
+
+//Solution
+
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    if(a==0) throw Error('Zero Error');
+    if(a<0) throw Error('Negative Error');
+    return 'YES';
+    }
