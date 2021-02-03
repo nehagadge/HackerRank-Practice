95cfcaba7debf31c1fe2706418b4591e027a807c
Neha Gadge <gadge.neha@gmail.com>
2021-02-03 15:46:49 -0800

    Create Day 6: Bitwise Operators.js

---
+++ 10 Days of JavaScript/Day 6: Bitwise Operators.js
@@ -0,0 +1,54 @@
+//Problem : https://www.hackerrank.com/challenges/js10-bitwise/problem
+
+Solution:
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+var getMaxLessThanK = function(n, k){
+    let maxvalue = 0;
+    for ( var i = 1 ;i < n ;i++)
+    {
+        for (var j = i+1 ; j < n+1; j++)
+        {
+            if ( Number(i & j) < k)
+            { 
+                if(Number(i & j) > maxvalue)
+                {
+                    maxvalue = i & j;
+                }
+            }
+        }
+    }
+    return maxvalue;
+}
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
