ba8f57b1f050888a2549777f9041ca8bf97e0ac0
Neha Gadge <gadge.neha@gmail.com>
2021-04-16 14:50:34 -0700

    Create Day 0: Weighted Mean.java

---
+++ 10 Days of Statistics/Day 0: Weighted Mean.java
@@ -0,0 +1,28 @@
+//Problem: https://www.hackerrank.com/challenges/s10-weighted-mean/problem
+
+//Solution:
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] x = new int[n];
+        for(int i=0; i<n; i++) {
+            x[i] = scan.nextInt();
+        }
+        int sum = 0, cnt = 0, w;
+        for(int i=0; i<n; i++) {
+            w = scan.nextInt();
+            sum += x[i] * w;
+            cnt += w;
+        }
+        System.out.printf("%.1f\n", (double)sum/cnt);
+    }
+}
